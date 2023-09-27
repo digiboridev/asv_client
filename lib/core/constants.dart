82470f046e020eceb5004376e692efe0dd98a934
digiborimusik <digiborimusik@gmail.com>
2023-09-27 22:34:24 +0300

    turn test

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,10 +1,35 @@
+import 'package:asv_client/core/env.dart';
+
 final kRTCPeerConfig = {
   'iceServers': [
-    {'urls': 'stun:stun.l.google.com:19302'},
-    {'urls': 'stun:stun1.l.google.com:19302'},
-    {'urls': 'stun:stun2.l.google.com:19302'},
-    {'urls': 'stun:stun3.l.google.com:19302'},
-    {'urls': 'stun:stun4.l.google.com:19302'},
+    // {'urls': 'stun:stun.l.google.com:19302'},
+    // {'urls': 'stun:stun1.l.google.com:19302'},
+    // {'urls': 'stun:stun2.l.google.com:19302'},
+    // {'urls': 'stun:stun3.l.google.com:19302'},
+    // {'urls': 'stun:stun4.l.google.com:19302'},
+    {
+      "urls": "stun:stun.relay.metered.ca:80",
+    },
+    {
+      "urls": "turn:a.relay.metered.ca:80",
+      "username": Env.turnU,
+      "credential": Env.turnC,
+    },
+    {
+      "urls": "turn:a.relay.metered.ca:80?transport=tcp",
+      "username": Env.turnU,
+      "credential": Env.turnC,
+    },
+    {
+      "urls": "turn:a.relay.metered.ca:443",
+      "username": Env.turnU,
+      "credential": Env.turnC,
+    },
+    {
+      "urls": "turn:a.relay.metered.ca:443?transport=tcp",
+      "username": Env.turnU,
+      "credential": Env.turnC,
+    },
   ],
   'sdpSemantics': 'unified-plan',
 };
--- lib/core/env.dart
+++ lib/core/env.dart
@@ -10,4 +10,8 @@ abstract class Env {
   static final apiUrl = _Env.apiUrl;
   @EnviedField(varName: 'APIURLDEV', obfuscate: true)
   static final apiUrlDev = _Env.apiUrlDev;
+  @EnviedField(varName: 'TURNU', obfuscate: true)
+  static final turnU = _Env.turnU;
+  @EnviedField(varName: 'TURNC', obfuscate: true)
+  static final turnC = _Env.turnC;
 }
